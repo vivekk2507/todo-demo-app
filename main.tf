14b32f609ec3f677773170636d10bb59068c07b0
vivekk2507 <vivekloggedin@gmail.com>
2024-06-17 09:38:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,43 +7,11 @@ variable "region" {
   type        = string
   default     = "ap-south-1"
 }
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.medium"
-}
-
-
-variable "jenkins_ip" {
-  description = "Jenkins' IP address for SSH access in CIDR notation (e.g., x.x.x.x/32)"
-  type        = string
-  default     = "43.204.143.128/32"
-}
 
 
 
 
 
 
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install -y openjdk-11-jdk wget tar",
-      "wget -O /tmp/app.tar.gz https://github.com/vivekk2507/todo-demo-app/archive/refs/heads/main.tar.gz",
-      "sudo mkdir -p /opt/app && sudo tar -xzvf /tmp/app.tar.gz -C /opt/app --strip-components=1",
-      "cd /opt/app",
-      "./mvnw package",
-      "nohup java -jar target/quarkus-app/quarkus-run.jar &"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"  # Replace with appropriate user for your AMI
-      private_key = file(var.keypair_name)
-      host        = aws_instance.example.public_ip
-    }
-  
-
-}
+    
 
