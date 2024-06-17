00eee33514a05e382c66a6d483b72397010622ae
vivekk2507 <vivekloggedin@gmail.com>
2024-06-17 07:31:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_instance" "example" {
     connection {
       type        = "ssh"
       user        = "ubuntu"  # Replace with appropriate user for your AMI
-      private_key = file("${path.module}/checkt.pem")
+      private_key = tls_private_key.checkt.private_key_pem
       host        = aws_instance.example.public_ip
     }
   }
