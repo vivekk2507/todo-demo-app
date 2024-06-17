deda393a44d85614ac613993b3e2bfb5e6321090
vivekk2507 <vivekloggedin@gmail.com>
2024-06-17 09:44:48 +0530

    Update and rename main.tf to provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  /*
+  access_key = "AKIA----------"
+  secret_key = "9dIj-------------------------"
+  */
+}
+
+
+
+
+    
+
