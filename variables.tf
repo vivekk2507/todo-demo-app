0115b0bfaebfd1fdb013bc842d78ebedac48301b
vivekk2507 <vivekloggedin@gmail.com>
2024-06-17 07:45:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
-variable "jenkins_ip" {
-  description = "Jenkins' IP address for SSH access in CIDR notation (e.g., x.x.x.x/32)"
-  type        = string
-  default     = "43.204.143.128/32"
-}
-
 variable "region" {
-  description = "AWS region"
+  description = "The AWS region to deploy the infrastructure"
   type        = string
-  default     = "ap-south-1"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "The type of instance to use"
   type        = string
-  default     = "t3.medium"
+}
+
+variable "jenkins_ip" {
+  description = "The IP address of the Jenkins server to allow SSH access"
+  type        = string
 }
 
 variable "keypair_name" {
-  description = "Name of the AWS key pair"
+  description = "The name of the key pair"
   type        = string
-  default     = "checkt"  # Replace with the actual key pair name
 }
