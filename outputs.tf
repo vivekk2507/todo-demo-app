8695daf9fff01fe1a94898d6fa68816903b3d2f6
vivekk2507 <vivekloggedin@gmail.com>
2024-06-17 07:39:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,4 @@ output "instance_public_ip" {
   description = "Public IP address of the deployed EC2 instance"
   value       = aws_instance.example.public_ip
 }
-output "private_key_path" {
-  value = local_file.private_key_pem.filename
-}
+
