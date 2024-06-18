e20cb220fd3f738077df7e1ef18b525eb2411c87
vivekk2507 <vivekloggedin@gmail.com>
2024-06-18 12:38:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  /*
-  access_key = "AKIA----------"
-  secret_key = "9dIj-------------------------"
-  */
+ 
+  access_key = "AKIA4MTWI3DCEPABZQZE"
+  secret_key = "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"
+
 }
 
 
