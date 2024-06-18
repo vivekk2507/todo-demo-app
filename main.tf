1fcf7ee47a5da61ddb0b39f45e3ead4bbcaa50fd
vivekk2507 <vivekloggedin@gmail.com>
2024-06-18 13:55:47 +0530

    Update and rename instance.tf to main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 # Creating key-pair on AWS using SSH-public key
 resource "aws_key_pair" "deployer" {
-  key_name   = var.key-name
+  key_name   = var.key_name
   public_key = file("${path.module}/my-key.pub")
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # Creating a security group to restrict/allow inbound connectivity
 resource "aws_security_group" "network-security-group" {
-  name        = var.network-security-group-name
+  name        = var.network_security_group_name
   description = "Allow TLS inbound traffic"
 
   ingress {
@@ -14,22 +18,27 @@ resource "aws_security_group" "network-security-group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  # Not recommended to add "0.0.0.0/0" instead we need to be more specific with the IP ranges to allow connectivity from.
+
   tags = {
     Name = "nsg-inbound"
   }
-}
 
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
 # Creating Ubuntu EC2 instance
 resource "aws_instance" "ubuntu-vm-instance" {
-  ami             = var.ubuntu-ami
-  instance_type   = var.ubuntu-instance-type
-  key_name        = aws_key_pair.deployer.key_name
+  ami                    = var.ubuntu_ami
+  instance_type          = var.ubuntu_instance_type
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.network-security-group.id]
+
   tags = {
     Name = "ubuntu-vm"
   }
 }
+
