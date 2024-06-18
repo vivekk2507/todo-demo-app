ca592ef9f38d2919d92f87179d5326de723ba4f9
vivekk2507 <vivekloggedin@gmail.com>
2024-06-18 13:26:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,14 @@
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
- 
- 
-
+  region = "us-east-1"  # Replace with your desired AWS region
 }
 
-
+// Debugging AWS credentials
+output "aws_credentials" {
+  value = {
+    access_key = aws_access_key.deployer.access_key
+    secret_key = aws_access_key.deployer.secret_key
+  }
+}
 
     
 
